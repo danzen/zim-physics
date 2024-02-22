8993c876b56b3ee3f1dc0a6fc9df6e399fefda1c
Dan Zen <door@danzen.com>
2024-02-21 23:21:57 -0500

    imported zim typescript

--- index.d.ts
+++ index.d.ts
@@ -2,25 +2,27 @@
 
 ///<reference types="./box2d.d.ts" />
 
+import {Boundary, Frame, Point, DisplayObject } from "zimjs"
+
 declare namespace zim {
 
     export class Physics {
-        constructor(gravity?: number, borders?: zim.Boundary | string, scroll?: boolean, frame?: zim.Frame);
-        borders(boundary?:zim.Boundary|string):this 
-        drag(array?:[zim.DisplayObject]):this 
-        noDrag(array?:[zim.DisplayObject]):this 
+        constructor(gravity?: number, borders?: Boundary | string, scroll?: boolean, frame?: Frame);
+        borders(boundary?:Boundary|string):this 
+        drag(array?:[DisplayObject]):this 
+        noDrag(array?:[DisplayObject]):this 
         pause(type?:boolean):this  
-        join(obj1:zim.DisplayObject, obj2:zim.DisplayObject, point1?:zim.Point, point2?:zim.Point, minAngle?:number, maxAngle?:number, type?:string):Box2D.b2Joint  
+        join(obj1:DisplayObject, obj2:DisplayObject, point1?:Point, point2?:Point, minAngle?:number, maxAngle?:number, type?:string):Box2D.b2Joint  
         break(joint:Box2D.b2Joint):void 
-        attach(control:zim.DisplayObject, obj:zim.DisplayObject):any
+        attach(control:DisplayObject, obj:DisplayObject):any
         unattach(id:any):void
         debug():this
         updateDebug():this
         removeDebug():this
         dispose():boolean
 
-        readonly world:Box2D.world
-        readonly frame:zim.Frame
+        readonly world:Box2D.b2World
+        readonly frame:Frame
         readonly scale:number
         readonly step:number
         timeStep:number
@@ -28,12 +30,12 @@ declare namespace zim {
         readonly paused:boolean
         scroll:boolean
         readonly ticker:Function 
-        readonly controlObj:zim.DisplayObject
-        readonly followObj:zim.DisplayObject
-        readonly borderTop:Box2D.body
-        readonly borderBottom:Box2D.body
-        readonly borderLeft:Box2D.body
-        readonly borderRight:Box2D.body
+        readonly controlObj:DisplayObject
+        readonly followObj:DisplayObject
+        readonly borderTop:Box2D.b2Body
+        readonly borderBottom:Box2D.b2Body
+        readonly borderLeft:Box2D.b2Body
+        readonly borderRight:Box2D.b2Body
     }
 }
 
